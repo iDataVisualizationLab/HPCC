59ee4d4eb7bbbbfeaf36e1f5b660b456450e976f
Tommy Dang <tommydang@coeits-MacBook-Pro.local>
2018-09-21 00:43:31 -0500

    Add viz

--- viz/myscripts/main.js
+++ viz/myscripts/main.js
@@ -552,6 +552,28 @@ function areaChart(){
     // Do nothing
 }
 
+function saveResults(){
+    var filename = "HPCC_requests";
+    var type = "json";
+    var str = JSON.stringify(hostResults);
+
+    var file = new Blob([str], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
 function pauseRequest(){
     clearInterval(interval2);
 }
