90911208ed994846a6274e692c9c54cb630436f9
Lino <lino.virgen96@gmail.com>
2019-01-23 16:48:10 -0600

    legend added

--- HiperView/HPCC_VR/scripts/ScatterPlot.js
+++ HiperView/HPCC_VR/scripts/ScatterPlot.js
@@ -47,6 +47,7 @@ function ScatterPlot( data, bin_size, scale )
         info.min = min;
         info.range = max - min;
         info.name = axis == 0 ? "x" : axis == 1 ? "y" : "z";
+        info.legend = "long long title";
 
         return info;
     }
@@ -75,10 +76,13 @@ function ScatterPlot( data, bin_size, scale )
                                     new THREE.Vector3( scale, 0, scale ),
                                     new THREE.Vector3( scale, 0, 0 ) );
         var box = new THREE.Line( box_geometry, box_material );
+
         grid.add( box );
         grid.add( setMarks() );
 
-        // marks
+        addAxisLegend( grid, x );
+        addAxisLegend( grid, y );
+        addAxisLegend( grid, z );
 
         function setMarks()
         {
@@ -88,21 +92,21 @@ function ScatterPlot( data, bin_size, scale )
             var ymark = [];
             var zmark = [];
 
-            var mark_material = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 1 } );
+            var mark_material = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 1 } );
             var mark_length = -1*scale/15;
             var line, mark_geometry, v, start, end;
-            
+
             for( var i=0; i<no_marks; i++ )
             {
                 v = scale * i/(no_marks-1);
 
                 // x marks
                 mark_geometry = new THREE.Geometry();
-                start = new THREE.Vector3( v, 0, 0 );
-                end = new THREE.Vector3( v, mark_length, 0 );
+                start = new THREE.Vector3( v, 0, scale );
+                end = new THREE.Vector3( v, mark_length, scale );
                 mark_geometry.vertices.push( start, end );
                 line = new THREE.Line( mark_geometry.clone(), mark_material.clone() );
-                addLegend( line, x, i, end );
+                addIntervalLegend( line, x, i, end );
                 xmark.push( line );
                 marks.add(xmark[i]);
 
@@ -112,7 +116,7 @@ function ScatterPlot( data, bin_size, scale )
                 end = new THREE.Vector3( 0, v, mark_length );
                 mark_geometry.vertices.push( start, end );
                 line = new THREE.Line( mark_geometry.clone(), mark_material.clone() );
-                addLegend( line, y, i, end );
+                addIntervalLegend( line, y, i, end );
                 ymark.push( line );
                 marks.add( ymark[i] );
 
@@ -122,13 +126,13 @@ function ScatterPlot( data, bin_size, scale )
                 end = new THREE.Vector3( mark_length, 0, v );
                 mark_geometry.vertices.push( start, end );
                 line = new THREE.Line( mark_geometry.clone(), mark_material.clone() );
-                addLegend( line, z, i, end );
+                addIntervalLegend( line, z, i, end );
                 zmark.push( line );
                 marks.add(zmark[i]);
 
             }
 
-            function addLegend( obj, axis, interval, pos )
+            function addIntervalLegend( obj, axis, interval, pos )
             {
 
                 var num = axis.min + axis.range * interval / (no_marks-1);
@@ -136,13 +140,13 @@ function ScatterPlot( data, bin_size, scale )
                 // console.log(legend);
 
                 var loader = new THREE.FontLoader();
-                var legend_material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+                var legend_material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 
                 loader.load( 'media/fonts/helvetiker_regular.typeface.json', function ( font ) {
 
                     var legend_geometry = new THREE.TextGeometry( num + "", {
                         font: font,
-                        size: scale/25,
+                        size: scale/30,
                         height: 0,
                         curveSegments: 12,
                         bevelEnabled: false
@@ -169,6 +173,40 @@ function ScatterPlot( data, bin_size, scale )
             }
 
             return marks;
+        }
+
+        function addAxisLegend( obj, axis )
+        {
+            var loader = new THREE.FontLoader();
+            var legend_material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+            loader.load( 'media/fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+                var legend_geometry = new THREE.TextGeometry( axis.legend, {
+                    font: font,
+                    size: scale/20,
+                    height: 0,
+                    curveSegments: 12,
+                    bevelEnabled: false
+                } );
+
+                var legend = new THREE.Mesh( legend_geometry, legend_material );
+
+                if( axis.name == "x" )
+                    legend.position.set( 0, scale/-4, scale );
+                if( axis.name == "y" )
+                {
+                    legend.position.set( 0, 0, scale/-4 );
+                    legend.rotation.set( 0, Math.PI/2, 0 );
+                }
+                if( axis.name == "z" )
+                {
+                    legend.position.set( scale/-4, 0, 0 );
+                    legend.rotation.set( Math.PI/-2, 0, Math.PI/-2 );
+                }
+
+                obj.add( legend );
+            } );
         }
 
         return grid;
