5270af66eb741101cd63774f8dc1364d405e024a
Zipexpo <nvtngan@hcmus.edu.vn>
2019-02-19 09:13:29 -0600

    add phasespace

---
+++ HiperView/HPCC_PhaseSpace/src/scripts/readDatav2.js
@@ -0,0 +1,36 @@
+var serviceList = ["Huff"];
+var serviceLists = [{text: "Huff", id: 0,
+    sub:[]}];
+var serviceListattr = ["arr"];
+var chosenService = 0;
+
+function readData(startCut,endCut) {
+     // return d3.json("src/data/Huffv2.json").then((data)=>{
+    return d3.json("src/data/Huffv3.json").then((data)=>{
+        // let timestep = d3.nest().key(d=>d.timestep).entries(data).length;
+        let timestep = data[0].values[0].value.length;
+        let timerange = d3.range(timestep).map(function(d) { return new Date(2007, d);});
+        let dataRead = [];
+        data.forEach(topics=>{
+            topics.values.forEach(d=>d.value.forEach(it=>{
+                let timestepIt = timerange[it.timestep];
+                if ((timestepIt>(startCut||0)) &&(timestepIt<(endCut||Infinity)))
+                dataRead.push({key: d.key.replace(/'|&/gi, ""),
+                        topic: topics.key,
+                        text: it.text,
+                        f: it.frequency,
+                        timestep: timestepIt,
+                        df: it.sudden});
+            }));
+        });
+        return dataRead;
+        // return data.map(d=>{
+        //     return {key: d.fam.replace("'", ""),
+        //     topic: d.topic,
+        //     text: d.text,
+        //     f: ~~d.frequency,
+        //     timestep: timerange[~~d.timestep],
+        //     df: ~~d.sudden}
+        // });
+    });
+}
