4b2b7eaa888a0761bfcae30c075bf00417b8de0d
Zipexpo <nvtngan@hcmus.edu.vn>
2019-02-25 20:55:59 -0600

    update

--- ParallelCoordinates/src/js/parallel.js
+++ ParallelCoordinates/src/js/parallel.js
@@ -560,7 +560,7 @@ function paths(selected, ctx, count) {
 
     shuffled_data = _.shuffle(selected);
 
-    data_table(shuffled_data.slice(0,25));
+    data_table(shuffled_data.slice(0,20));
 
     ctx.clearRect(0,0,w+1,h+1);
 
@@ -801,28 +801,34 @@ function search(selection,str) {
 }
 
 $( document ).ready(function() {
+    $('.tabs').tabs();
+    $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
 
-    let comboBox = d3.select("#groupName");
+    let comboBox = d3.select("#listvar");
     let listOption = d3.merge(serviceLists.map(d=>d.sub.map(e=>{return {service: d.text, arr:serviceListattrnest[d.id].sub[e.id], text:e.text}})));
     listOption.push({service: 'Rack', arr:'rack', text:'Rack'});
-    comboBox.select('.dropdown-content')
-        .selectAll('a').data(listOption).join('a')
-        .text(d=>d.text)
-        .on('click',d=>{
-            comboBox.select('.dropbtn').text(d.text);
-            if (d.arr==='rack'){
-                selectedService = null;
-                svgLengend.style('display','none');
-            }else {
-                selectedService = d.arr;
-                setColorsAndThresholds(d.service);
-                drawLegend(d.service, arrThresholds, arrColor, dif);
-                svgLengend.style('display',null);
-            }
-            changeGroupTarget(d.arr);
-            legend = create_legend(colors,brush);
-            brush();
-        });
+    comboBox
+        .selectAll('li').data(listOption)
+        .join(enter => enter.append("li") .attr('tabindex','0').append("a")
+                .attr('href',"#"))
+        .text(d=>{console.log(d); return d.text})
+        .on('click',changeVar);
+
     d3.select("#DarkTheme").on("click",switchTheme);
 });
+function changeVar(d){
+    $('#groupName').text(d.text);
+    if (d.arr==='rack'){
+        selectedService = null;
+        svgLengend.style('display','none');
+    }else {
+        selectedService = d.arr;
+        setColorsAndThresholds(d.service);
+        drawLegend(d.service, arrThresholds, arrColor, dif);
+        svgLengend.style('display',null);
+    }
+    changeGroupTarget(d.arr);
+    legend = create_legend(colors,brush);
+    brush();
+}
