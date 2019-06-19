247fa4da0acba47241648c2fdf1d9f741e8e63c7
Zipexpo <nvtngan@hcmus.edu.vn>
2019-06-19 00:38:03 -0500

    change filesave prevent error
    wroking on #55

--- HiperView/myscripts/setting.js
+++ HiperView/myscripts/setting.js
@@ -4,12 +4,15 @@ var serviceList = ["Temperature","Job_load","Memory_usage","Fans_speed","Power_c
 var serviceList_selected = ["Temperature","Job_load","Memory_usage","Fans_speed","Power_consum"];
 
 var serviceListattr = ["arrTemperature","arrCPU_load","arrMemory_usage","arrFans_health","arrPower_usage","arrJob_scheduling"];
-var serviceLists = [{text: "Temperature", id: 0, enable:true,
-    sub:[{text: 'CPU1 Temp', id: 0, enable:true},{text: 'CPU2 Temp', id: 1, enable:true},{text: 'Inlet Temp', id: 2, enable:true}]},
-    {text: "Job_load", id: 1, enable:true ,sub:[{text: 'Job load', id: 0, enable:true}]},
-    {text: "Memory_usage", id: 2 , enable:true ,sub:[{text: 'Memory usage', id: 0, enable:true}]},
-    {text: "Fans_speed", id: 3 , enable:true ,sub:[{text: 'Fan1 speed', id: 0, enable:true},{text: 'Fan2 speed', id: 1, enable:true},{text: 'Fan3 speed', id: 2, enable:true},{text: 'Fan4 speed', id: 3, enable:true}]},
-    {text: "Power_consum", id: 4 , enable:true ,sub:[{text: 'Power consumption', id: 0, enable:true}]}];
+var serviceLists = [{"text":"Temperature","id":0,"enable":true,"sub":[{"text":"CPU1 Temp","id":0,"enable":true,"idroot":0,"angle":5.834386356666759,"range":[3,98]},{"text":"CPU2 Temp","id":1,"enable":true,"idroot":0,"angle":0,"range":[3,98]},{"text":"Inlet Temp","id":2,"enable":true,"idroot":0,"angle":0.4487989505128276,"range":[3,98]}]},{"text":"Job_load","id":1,"enable":true,"sub":[{"text":"Job load","id":0,"enable":true,"idroot":1,"angle":1.2566370614359172,"range":[0,10]}]},{"text":"Memory_usage","id":2,"enable":true,"sub":[{"text":"Memory usage","id":0,"enable":true,"idroot":2,"angle":1.8849555921538759,"range":[0,99]}]},{"text":"Fans_speed","id":3,"enable":true,"sub":[{"text":"Fan1 speed","id":0,"enable":true,"idroot":3,"angle":2.4751942119192307,"range":[1050,17850]},{"text":"Fan2 speed","id":1,"enable":true,"idroot":3,"angle":2.9239931624320583,"range":[1050,17850]},{"text":"Fan3 speed","id":2,"enable":true,"idroot":3,"angle":3.372792112944886,"range":[1050,17850]},{"text":"Fan4 speed","id":3,"enable":true,"idroot":3,"angle":3.8215910634577135,"range":[1050,17850]}]},{"text":"Power_consum","id":4,"enable":true,"sub":[{"text":"Power consumption","id":0,"enable":true,"idroot":4,"angle":4.71238898038469,"range":[0,200]}]}];
+var serviceFullList = serviceLists2serviceFullList(serviceLists);
+function serviceLists2serviceFullList (serviceLists){
+    let temp = [];
+    serviceLists.forEach(s=>s.sub.forEach(sub=>{
+        sub.idroot = s.id;
+        temp.push(sub);}));
+    return temp;
+}
 var serviceListattrnest = [
     {key:"arrTemperature", sub:["CPU1 Temp","CPU2 Temp","Inlet Temp"]},
     {key:"arrCPU_load", sub:["Job load"]},
--- HiperView/myscripts/store.js
+++ HiperView/myscripts/store.js
@@ -1,16 +1,14 @@
 // store
 var chosenService = 0;
+const variablesNames = ['serviceList','serviceLists','serviceListattr','serviceListattrnest']
 var conf={};
-conf.serviceList = serviceList;
-conf.serviceLists = serviceLists;
-conf.serviceListattr = serviceListattr;
-conf.serviceListattrnest = serviceListattrnest;
+variablesNames.forEach(d=>conf[d] = window[d]);
 
 function Loadtostore() {
-    checkConf('serviceList');
-    checkConf('serviceLists');
-    checkConf('serviceListattr');
-    checkConf('serviceListattrnest');
+    variablesNames.forEach(d=>{ window[d] = checkConf(d)});
+}
+function DeleteAllstore() {
+    variablesNames.forEach(d=>refeshConf(d));
 }
 //***********************
 Loadtostore();
@@ -21,12 +19,17 @@ function checkConf(namekey) {
     var retrievedObject = localStorage.getItem(namekey);
     if (retrievedObject!=null&&retrievedObject!==undefined&&retrievedObject!=="undefined") {
         conf[namekey] = JSON.parse(retrievedObject);
-        console.log('retrievedObject: ', JSON.parse(retrievedObject));
+        return conf[namekey];
     } else {
         saveConf(namekey);
+        return conf[namekey];
     }
 }
 
 function saveConf(namekey) {
     localStorage.setItem(namekey, JSON.stringify(conf[namekey]));
+}
+
+function refeshConf(namekey) {
+    localStorage.removeItem(namekey);
 }
