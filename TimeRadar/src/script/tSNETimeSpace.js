880066998891bd47ae20bf641cbce94a1bfd169b
Zipexpo <nvtngan@hcmus.edu.vn>
2019-12-16 13:33:56 -0600

    radar dispplay

--- TimeRadar/src/script/tSNETimeSpace.js
+++ TimeRadar/src/script/tSNETimeSpace.js
@@ -20,6 +20,15 @@ d3.tsneTimeSpace = function () {
                 epsilon: 20, // epsilon is learning rate (10 = default)
                 perplexity: 1000, // roughly how many neighbors each point influences (30 = default)
                 dim: 2, // dimensionality of the embedding (2 = default)
+            },radaropt : {
+                // summary:{quantile:true},
+                mini:true,
+                levels:6,
+                gradient:true,
+                w:40,
+                h:40,
+                showText:false,
+                margin: {top: 0, right: 0, bottom: 0, left: 0},
             }
         },
         runopt = {},
@@ -30,9 +39,10 @@ d3.tsneTimeSpace = function () {
     // grahic 
     let background_canvas,background_ctx,front_canvas,front_ctx,svg;
     //----------------------color----------------------
-    let colorCluster  = d3.scaleOrdinal().range(d3.schemeCategory10);
+    let createRadar = _.partialRight(createRadar_func,graphicopt.radaropt,colorscale);
     master.init = function(arr,cluster) {
         datain = arr;
+        handle_data(datain);
         background_canvas = document.getElementById("tsneScreen");
         background_canvas.width  = graphicopt.widthG();
         background_canvas.height = graphicopt.heightG();
@@ -87,6 +97,7 @@ d3.tsneTimeSpace = function () {
                     break;
                 case "stable":
                     isBusy = false;
+                    render(solution,true);
                     tsne.terminate();
                 default:
                     break;
@@ -94,13 +105,15 @@ d3.tsneTimeSpace = function () {
         })
 
         return master;
-        function render (solution){
+        function render (solution,isradar){
+            createRadar = _.partialRight(createRadar_func,graphicopt.radaropt,colorscale)
             background_ctx.clearRect(0, 0, graphicopt.widthG(), graphicopt.heightG());
             if(filter_by_name&&filter_by_name.length)
                 front_ctx.clearRect(0, 0, graphicopt.widthG(), graphicopt.heightG());
             let path = {};
             solution.forEach(function(d, i) {
-                const target = arr[i];
+                const target = datain[i];
+                target.__metrics.position = d;
                 if (!path[target.name])
                     path[target.name] = [];
                 path[target.name].push({name:target.name,key:target.timestep,value:d});
@@ -113,7 +126,9 @@ d3.tsneTimeSpace = function () {
                     drawline(background_ctx,target, d);
                 }
                 hightlight_render_single(target, d);
-            })
+            });
+
+
             let linepath = svg.selectAll('path').data(d3.values(path).map(d=>d.sort((a,b)=>a.t-b.t)));
             linepath
                 .enter().append('path')
@@ -127,16 +142,34 @@ d3.tsneTimeSpace = function () {
                 }).on('mouseleave',d=>{
                     d3.selectAll('.h'+d[0].name).dispatch('mouseleave');
                 })
-            // datapoint= bg.selectAll(".linkLinegg").interrupt().data(d => d.timeline.clusterarr.map((e,i) => {
-            //     temp = _.cloneDeep(newdata.find(n => n.name === e.cluster));
-            //     temp.name = e.cluster;
-            //     temp.timestep = e.timestep;
-            //     if(!i)
-            //         temp.hide = true;
-            //     return temp;
-            // }),d=>d.name+d.timestep);
+            if(isradar) {
+                let datapoint = svg.selectAll(".linkLinegg").interrupt().data(d => datain.map(e => e.__metrics), d => d.name + d.timestep);
+                datapoint.exit().remove();
+                let datapoint_n = datapoint.enter().append('g')
+                    .attr('class', 'linkLinegg timeline');
+                datapoint_n.each(function (d, i) {
+                    createRadar(d3.select(this).select('.linkLineg'), d3.select(this), d, {colorfill: true}).classed('hide', d.hide);// hide 1st radar
+                });
+
+                datapoint_n.merge(datapoint).attr('transform', function (d) {
+                    return `translate(${xscale(d.position[0])},${yscale(d.position[1])})`
+                })
+            }
         }
     };
+
+    function handle_data(data){
+        data.forEach(d=>{
+            d.__metrics = d.map((m,i)=>{
+                return {axis: serviceFullList[i].text, value: m}
+            });
+            d.__metrics.name = d.clusterName;
+            d.__metrics.orname = d.name;
+            d.__metrics.timestep = d.timestep;
+        })
+    }
+
+
 
     function drawline(ctx,target, d) {
         let nexttime = solution[maptimestep[target.name][target.timestep]];
@@ -223,16 +256,17 @@ d3.tsneTimeSpace = function () {
         }
 
     };
-    master.graphicopt = function (_) {
+    master.graphicopt = function (__) {
         //Put all of the options into a variable called graphicopt
         if (arguments.length) {
-            for (let i in _) {
-                if ('undefined' !== typeof _[i]) {
-                    graphicopt[i] = _[i];
+            for (let i in __) {
+                if ('undefined' !== typeof __[i]) {
+                    graphicopt[i] = __[i];
                 }
             }
             if (graphicopt.radaropt)
-                graphicopt.radaropt.schema = schema
+                graphicopt.radaropt.schema = serviceFullList;
+            createRadar = _.partialRight(createRadar_func,graphicopt.radaropt,colorscale)
             return master;
         }else {
             return graphicopt;
@@ -242,10 +276,6 @@ d3.tsneTimeSpace = function () {
 
     master.solution = function (_) {
         return solution;
-    };
-    
-    master.data = function (_) {
-        return arguments.length ? (dataraw = _, handle_data(),master) : dataRaw;
     };
     
     master.color = function (_) {
@@ -271,6 +301,7 @@ function handle_data_tsne(tsnedata) {
         let count = 0;
         sampleS.timespan.forEach((t, i) => {
             let index = axis_arr[i].cluster;
+            axis_arr[i].clusterName = cluster_info[index].name
             // timeline precalculate
             if (!(lastcluster !== undefined && index === lastcluster) || runopt.suddenGroup&& calculateMSE_num(lastdataarr,axis_arr[i])>cluster_info[axis_arr[i].cluster].mse*runopt.suddenGroup) {
                 lastcluster = index;
