3430bccad63842dfa2ccdfc49444fe4458a9da54
nvtngan <nvtngan@hcmus.edu.vn>
2020-03-06 03:47:55 -0600

    Update jobMap.js

--- TimeRadar/src/script/jobMap.js
+++ TimeRadar/src/script/jobMap.js
@@ -525,7 +525,7 @@ let JobMap = function() {
 
 
         if (!runopt.compute.bundle) {
-            const radaropt = {colorfill: colorfill, size: Math.max((scaleNode_y_middle(1) - scaleNode_y_middle(0)) * 2,12)};
+            const radaropt = {colorfill: colorfill, size: Math.min(Math.max((scaleNode_y_middle(1) - scaleNode_y_middle(0)) * 2,12),50)};
             let datapoint;
             if (!runopt.suddenGroup) {
                 datapoint= bg.selectAll(".linkLinegg").interrupt().data(d => d.timeline.clusterarr.map((e,i) => {
