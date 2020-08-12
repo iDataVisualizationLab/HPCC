8d44d77244c29bd746c9e229d570d2f23a0a5ef8
nvtngan <nvtngan@hcmus.edu.vn>
2020-08-12 10:23:50 -0500

    update control

--- jobviewer/src/js/menu.js
+++ jobviewer/src/js/menu.js
@@ -1,4 +1,20 @@
 function initMenu(){
     $('#legendHolder').draggable({ handle: ".card-header" ,containment: "parent", scroll: false });
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+    initSideBar()
+}
+function minimizeToolbar(){
+    extractToolbar("#controlOnToolbar",true);
+    extractToolbar("#analyticOnToolbar",true);
+    d3.select("#legendHolder").classed("hide",true);
+}
+function extractToolbar(id,status){
+    const node = $(id);
+    if (status){
+        node.appendTo('#'+node.attr('data-minimize'));
+        d3.select('#'+node.attr('data-maximize')).classed('hide',true)
+    }else{
+        node.appendTo('#'+node.attr('data-maximize'));
+        d3.select('#'+node.attr('data-maximize')).classed('hide',false)
+    }
 }
--- jobviewer/src/js/sidebar.js
+++ jobviewer/src/js/sidebar.js
@@ -1,7 +1,16 @@
 $('.sidebar').hover(hoverSidebarIn,hoverSidebarOut);
+function initSideBar(){
+    $('#sidenav .collapse').on('show.bs.collapse',function(){
+        extractToolbar($(this).attr('data-action'),true);
+    }).on('hidden.bs.collapse',function(){
+        if(!d3.select('#'+$($(this).attr('data-action')).attr('data-minimize')).classed('hide')){
+            extractToolbar($(this).attr('data-action'),false);
+        }
+    })
+}
 function hoverSidebarIn() {
     $(this).toggleClass( "active", true);
-    document.documentElement.style.setProperty('--sideNavwidth', 250 + "px");
+    document.documentElement.style.setProperty('--sideNavwidth', 330 + "px");
 }
 function hoverSidebarOut(){
     $(this).toggleClass( "active", false);
