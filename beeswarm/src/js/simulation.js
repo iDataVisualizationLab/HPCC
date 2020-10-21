aeb433b906436edcae1c652af81dae8f6cec478f
Zipexpo <nvtngan@hcmus.edu.vn>
2020-10-21 15:30:55 -0500

    Update simulation.js

--- beeswarm/src/js/simulation.js
+++ beeswarm/src/js/simulation.js
@@ -11,6 +11,7 @@ class Simulation {
     onTimeChange=[];
     onDataChange=[];
     onUpdateTime=[];
+    userDict={};
     onStartQuery=()=>{};
     constructor(url) {
         this.isRealTime = !url;
@@ -18,6 +19,9 @@ class Simulation {
             let updatePromise=(_.isString(url)?d3.json(url):url).then((data) => {
                 data.time_stamp = data.time_stamp.map(d=>new Date(d/1000000));
                 d3.keys(data.jobs_info).forEach(jID=>{
+                    if (!this.userDict[data.jobs_info[jID].user_name])
+                        this.userDict[data.jobs_info[jID].user_name] = 'user'+d3.keys(this.userDict).length;
+                    data.jobs_info[jID].user_name = this.userDict[data.jobs_info[jID].user_name];
                     data.jobs_info[jID].node_list = data.jobs_info[jID].node_list.map(c=>c.split('-')[0]);
                     if(data.jobs_info[jID].start_time>9999999999999)
                     {data.jobs_info[jID].start_time = data.jobs_info[jID].start_time/1000000
@@ -136,6 +140,9 @@ class Simulation {
         return d3.json(url).then(function(data){
             data.time_stamp=data.time_stamp.map(e=>new Date(e/1000000));
             d3.keys(data.jobs_info).forEach(jID=>{
+                if (!self.userDict[data.jobs_info[jID].user_name])
+                    self.userDict[data.jobs_info[jID].user_name] = 'user'+d3.keys(self.userDict).length;
+                data.jobs_info[jID].user_name = self.userDict[data.jobs_info[jID].user_name];
                 data.jobs_info[jID].node_list = data.jobs_info[jID].node_list.map(c=>c.split('-')[0]);
                 if(data.jobs_info[jID].start_time>9999999999999)
                 {data.jobs_info[jID].start_time = data.jobs_info[jID].start_time/1000000
