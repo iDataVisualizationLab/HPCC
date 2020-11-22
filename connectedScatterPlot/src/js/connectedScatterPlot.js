fb475cfaf45fc9f373147739d8a013088123cc84
Zipexpo <nvtngan@hcmus.edu.vn>
2020-11-21 23:50:11 -0600

    Update connectedScatterPlot.js

--- connectedScatterPlot/src/js/connectedScatterPlot.js
+++ connectedScatterPlot/src/js/connectedScatterPlot.js
@@ -60,6 +60,11 @@ let ConnectedScatterPlot = function (){
     let colorP = d3.scaleLinear()
         .range(['black','green'])
         .interpolate(d3.interpolateHcl);
+    let userIcon = {close:`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+</svg>`,open:`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-dash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5-.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+</svg>`}
     let layout={};
     master.reset = function(){
 
@@ -247,10 +252,13 @@ let ConnectedScatterPlot = function (){
                 update(d);
             });
 
-        nodeEnter.filter(d=>!d.data.svg).append("circle")
+        nodeEnter.filter(d=>!d.data.svg && d.data.type!=='user').append("circle")
             .attr("r", 2.5)
             .attr("fill", d => d._children ? "#555" : "#999")
             .attr("stroke-width", 10);
+        nodeEnter.filter(d=>d.data.type==='user').append("svg")
+            .attr("fill", d => d._children ? "#555" : "#999")
+            .html(d.children ?userIcon.open:userIcon.close);
         nodeEnter.filter(d=>d.data.svg)
             .selectAll('g.scatter')
             .data(d=> d.data.svg)
@@ -389,7 +397,7 @@ function handle_data_timeArc () {
     const dataArray = [];
     userlist.forEach(u=>{
         const datauser = {
-            name: u, children:
+            name: u, type:"user", children:
                 Layout.usersStatic[u].jobMain
                     .map(j => {
                         if (Layout.jobsStatic[j])
