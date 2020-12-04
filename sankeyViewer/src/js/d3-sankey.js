0a7ffa4c3698cc9b2fb7ebc3ba6af46bd6cd8ef2
Zipexpo <nvtngan@hcmus.edu.vn>
2020-12-04 16:31:06 -0600

    Update d3-sankey.js

--- sankeyViewer/src/js/d3-sankey.js
+++ sankeyViewer/src/js/d3-sankey.js
@@ -186,20 +186,24 @@ function Sankey() {
   }
 
   function computeNodeDepths({nodes}) {
-    const n = nodes.length;
-    let current = new Set(nodes);
-    let next = new Set;
-    let x = 0;
-    while (current.size) {
-      for (const node of current) {
-        node.depth = x;
-        for (const {target} of node.sourceLinks) {
-          next.add(target);
+    if (nodes[0].layer!==undefined){
+      nodes.forEach(n=>n.depth = n.layer);
+    }else {
+      const n = nodes.length;
+      let current = new Set(nodes);
+      let next = new Set;
+      let x = 0;
+      while (current.size) {
+        for (const node of current) {
+          node.depth = x;
+          for (const {target} of node.sourceLinks) {
+            next.add(target);
+          }
         }
+        if (++x > n) throw new Error("circular link");
+        current = next;
+        next = new Set;
       }
-      if (++x > n) throw new Error("circular link");
-      current = next;
-      next = new Set;
     }
   }
 
