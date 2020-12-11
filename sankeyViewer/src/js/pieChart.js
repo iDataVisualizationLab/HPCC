a6b2813e86cae1f81f7a37ca8a3a872b236dcfc6
Zipexpo <nvtngan@hcmus.edu.vn>
2020-12-11 08:15:27 -0600

    Update pieChart.js

--- sankeyViewer/src/js/pieChart.js
+++ sankeyViewer/src/js/pieChart.js
@@ -1,8 +1,8 @@
 function UserPie(){
     let graphicopt = {
-        margin: {top: 20, right: 50, bottom: 20, left: 50},
+        margin: {top: 0, right: 50, bottom: 0, left: 50},
         width: 300,
-        height: 200,
+        height: 250,
         'max-height': 500,
         scalezoom: 1,
         zoom:d3.zoom(),
