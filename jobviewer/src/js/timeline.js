8ad80e26d394408776464097c8473db382074ab8
Zipexpo <nvtngan@hcmus.edu.vn>
2021-01-06 15:22:00 -0600

    Update timeline.js

--- jobviewer/src/js/timeline.js
+++ jobviewer/src/js/timeline.js
@@ -55,11 +55,12 @@ class Timeline{
             .style("width", "calc(100% - 30px - 200px)");
         this.timelineHolder.append('div')
             .attr('class','input-group row justify-content-center')
-            .html(`<span class="input-group-text" id="addon-wrapping">Current time</span> <button class="btn btn-outline-secondary timeDecrease" type="button"><</button><input type="text" class="timeInput" aria-label="Recipient's username with two button addons">
+            .html(`<span class="input-group-text" id="addon-wrapping">Current time</span> <button class="btn btn-outline-secondary timeDecrease" type="button"><</button><input type="text" class="timeInput" >
   <button class="btn btn-outline-secondary timeIncrease" type="button">></button>`);
         this.timelineHolder.select('button.timeIncrease').on('click',()=>onChangeTimeByStep(1));
         this.timelineHolder.select('button.timeDecrease').on('click',()=>onChangeTimeByStep(-1));
         this.timelineText = this.timelineHolder.select('input.timeInput')
+            .on('click',function(){self.pause()})
             .on('change',function(){self.step(new Date(this.value))});
 
 
