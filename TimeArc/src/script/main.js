71ab7ad42cd299a6c35cc0f7b6df57fcc126c70d
Zipexpo <nvtngan@hcmus.edu.vn>
2021-01-22 17:01:24 -0600

    Update main.js

--- TimeArc/src/script/main.js
+++ TimeArc/src/script/main.js
@@ -321,7 +321,7 @@ function makedataworker(){
 }
 var vizMode = 'timeArc'; // 0 timeradar, 1 tsne, 2 pca, 3 umap
 let handle_data_TimeSpace;
-let timeArc = ConnectedScatterPlot()
+let timeArc = TimeArcSetting();
 let mainviz = timeArc;
 function initDataWorker(){
     getDataWorker.postMessage({action:"init",value:{
