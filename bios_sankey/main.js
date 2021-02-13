700e0c9161c62c72db4e5798b40614c98e657600
Zipexpo <nvtngan@hcmus.edu.vn>
2021-02-13 15:34:57 -0600

    update

--- bios_sankey/main.js
+++ bios_sankey/main.js
@@ -1,21 +1,51 @@
 
 d3.csv('src/data/all_bios_info.csv').then(data=>{
-    const width = 3075, height = 720;
-    const {columns} = getColumnData(data);
-
-    const keys = columns.map((d,i)=>{
-        return d.key});
-    d3.select('#colorBy').selectAll('option').data(keys)
-        .join('option')
-        .attr('value',(d,i)=>i)
-        .text(d=>d);
-    d3.select('#colorBy').on('change',function(){
-        const choice = this.selectedIndex;
-        Sankey.draw(data,[columns[choice],...columns.slice(0,choice),...columns.slice(choice+1)]);
+    const width = 3075, height = 680;
+    const {_columns,_columnsObj} = getColumnData(data);
+    let columns = _columns.filter(d=>{
+        d.values=d.values.filter(e=>e.key!=='');
+        if (d.values.length<=2 || d.values.length>=data.length/2){
+            d.selected = false;
+            return false;
+        }
+        d.selected = true;
+        return true;
     });
-    const Sankey = drawSankey('#svg_main',{width,height})
-    Sankey.draw(data,[columns[0],...columns.slice(1)])
-})
+
+    d3.select('#axis').selectAll('option').data(_columns)
+        .join('option')
+        .attr('value',(d,i)=>d.key)
+        .attr('selected',d=>d.selected?'selected':null)
+        .text(d=>d.key+` (${d.values.length})`);
+
+    $('#axis').multiselect({enableFiltering: true,
+        includeFilterClearBtn: false,
+        maxHeight: 600,
+        includeSelectAllOption: true,
+        onChange: function(options, checked) {
+            columns = options.parent().val().map(d=>_columnsObj[d]);
+            updateAxisChoice();
+            Sankey.draw(data,[columns[0],...columns.slice(1)]);
+        }
+    });
+
+
+    console.log(columns.length)
+    updateAxisChoice();
+    const Sankey = drawSankey('#svg_main',{width,height});
+    Sankey.draw(data,[columns[0],...columns.slice(1)]);
+    function updateAxisChoice(){
+        d3.select('#colorBy').selectAll('option').data(columns)
+            .join('option')
+            .attr('value',(d,i)=>i)
+            .text(d=>d.key);
+        d3.select('#colorBy').on('change',function(){
+            const choice = this.selectedIndex;
+            Sankey.draw(data,[columns[choice],...columns.slice(0,choice),...columns.slice(choice+1)]);
+        });
+    }
+});
+
 
 
 function handleData(data,keys){
@@ -68,7 +98,7 @@ function drawSankey (divID,{width,height}){
         .nodeSort(null)
         .linkSort(null)
         .nodeWidth(4)
-        .nodePadding(20)
+        .nodePadding(10)
         .extent([[0, 5], [width, height - 5]])
 
     // const color = d3.scaleOrdinal(graph.keys, d3.schemeCategory10).unknown("#ccc")
@@ -91,6 +121,11 @@ function drawSankey (divID,{width,height}){
 
     const master = {};
     master.draw=(data,columns)=>{
+        width = 83*columns.length;
+        sankey.extent([[0, 5], [width, height - 5]]);
+        svg.attr("width", width);
+
+
         const keys = columns.map(k=>k.key);
         const graph = handleData(data,keys);
         const {nodes, links} =  sankey({
@@ -179,44 +214,11 @@ function getColumnData(data){
 
     let dataViz = Object.values(keysObject);
     dataViz.forEach(d=>{
-        let count = 0;
         d.values = Object.keys(d.values).map(e=>{
-            const item = {key:e,start:count,value:d.values[e]};
-            count+= d.values[e];
-            item.end = count;
+            const item = {key:e,value:d.values[e]};
             return item;
         })
     });
     // const color = {};
-    dataViz=dataViz.filter(d=>{
-        d.values=d.values.filter(e=>e.key!=='');
-        if (d.values.length<=2 || d.values.length>=data.length/2)
-            return false;
-        // const colorIn = d3.scaleOrdinal()
-        //     .range(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#bcbd22", "#17becf"]);
-        let count = 0;
-        d.values.sort((a,b)=>b.value-a.value)
-        d.values.forEach(e=>{
-            e.start = count;
-            count+=e.value;
-            e.end = count;
-            // color[e.key] = colorIn(e.key);
-        });
-        return true;
-    });
-    // color[""]="#7f7f7f";
-    // let xscale = d3.scaleBand().domain(dataViz.map(d => d.key)).range([0,width]).padding(0.1);
-    // let yscale = d3.scaleLinear().domain([0,data.length]).range([height,0]);
-    //
-    // svg.selectAll('g.barGroup').data(dataViz)
-    //     .join('g').attr('class','barGroup')
-    //     .attr('transform',d=>`translate(${xscale(d.key)},${yscale.range()[1]})`)
-    //     .selectAll('rect').data(d=>d.values)
-    //     .join('rect')
-    //     .attr('y',d=>yscale(d.end))
-    //     .attr('height',d=>-yscale(d.end)+yscale(d.start))
-    //     .attr("width", xscale.bandwidth())
-    //     .style('fill',d=>color[d.key]);
-    // return {columns:dataViz,color};
-    return {columns:dataViz};
+    return {_columns:dataViz,_columnsObj:keysObject};
 }
