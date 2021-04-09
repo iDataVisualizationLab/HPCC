f393965db97c79046b98d676a3f22c2b8893e5f2
Zipexpo <nvtngan@hcmus.edu.vn>
2021-04-08 19:06:03 -0500

    Update sampledata.js

--- JobSqueue/src/script/angular/components/databasetable/sampledata.js
+++ JobSqueue/src/script/angular/components/databasetable/sampledata.js
@@ -9,13 +9,13 @@ angular.module('hpccApp').constant('SampleData', [
         group:"sample",
         formatType:'json',
         type:'json',
-        customSetting: {definedType:{"JOBID":"text","TIME":"duration","TIME_LEFT":"duration","START_TIME":"time"},
+        customSetting: {definedType:{"ARRAY_JOB_ID":"text","TIME":"duration","TIME_LEFT":"duration","START_TIME":"time"},
         preprocess:{"MIN_MEMORY":(s)=>{const v = s.split(" ")[0].trim(); if(v[v.length-1]==='M')
             return +v.slice(0,v.length-1)/1000
                 else
                 return +v.slice(0,v.length-1)
         }},
-        disableAxis: {"NODELIST":true,"JOBID":true,"USER":true},
+        disableAxis: {"NODELIST":true,"ARRAY_JOB_ID":true,"USER":true},
         initAxis: "STATE",
         // "TIME":s=>moment.duration(s.replace('-',' '))._milliseconds,
         // "TIME_LEFT":s=>moment.duration(s.replace('-',' '))._milliseconds,
