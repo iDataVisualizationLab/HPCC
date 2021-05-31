e48b1e3ba794c8ad06fa6d9ad2920c1bb7b3540a
Zipexpo <nvtngan@hcmus.edu.vn>
2021-05-31 02:37:42 -0500

    Update MapSetting.js

--- PowerMap/src/js/MapSetting.js
+++ PowerMap/src/js/MapSetting.js
@@ -1183,6 +1183,27 @@ let MapSetting = function () {
                         d.value[step+1] = computersObj[alldataPoint[i].name].drawData[j].value[step+1];
                 });
             }
+        }else if (runopt.highlightStream==='suddenChange'){
+            // find top 10
+            let alldataPoint = [];
+            computers.forEach(d=>{
+                d.highlightData = d.drawData.map(d=>{
+                    return {value:[],color:d.color,up:d.up}
+                });
+                scheme.data.tsnedata[d.key].forEach(e=>{alldataPoint.push(e)})
+            });
+            alldataPoint.sort((a,b)=>b[serviceSelected]-a[serviceSelected]);
+            for (let i =0; i<10;i++){
+                const step = alldataPoint[i].timestep;
+                console.log(alldataPoint[i])
+                computersObj[alldataPoint[i].name].highlightData.forEach((d,j)=>{
+                    if (computersObj[alldataPoint[i].name].drawData[j].value[step-1])
+                        d.value[step-1] = computersObj[alldataPoint[i].name].drawData[j].value[step-1];
+                    d.value[step] = computersObj[alldataPoint[i].name].drawData[j].value[step];
+                    if (computersObj[alldataPoint[i].name].drawData[j].value[step+1])
+                        d.value[step+1] = computersObj[alldataPoint[i].name].drawData[j].value[step+1];
+                });
+            }
         }else {
             computers.forEach(d=>d.highlightData = undefined);
         }
